dbec1f65d389a68f19400462778d0bd8c58cb6fb
unknown <dipjal5678@gmai.com>
2023-07-30 12:47:13 +0545

    added morgan middlewarer for console.log (exercise 3.7)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
+
 let persons = [
   {
     id: 1,
@@ -22,9 +26,57 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+const currentTime = new Date().toString();
+
+const entries = persons.length;
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const responseMessage = `<p>Phonebook has info of ${entries} people</p>
+      <p>${currentTime}</p>`;
+
+  response.send(responseMessage);
+});
+
+app.post("api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    return response.status(404).json({ error: "name and number are requird" });
+  }
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  console.log("seperate id");
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).json({ error: "person not found" });
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  console.log("deleted");
+
+  response.status(204).end();
+});
+
+app.post("/api/persons/:id", (request, response) => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+
+  const person = request.body;
+  person.id = maxId + 1;
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
