6ca824d86b8ed245d5383a948f7594e21abbf936
unknown <dipjal5678@gmai.com>
2023-08-03 09:43:39 +0545

    data fetcehd to frontend i.e. part2 in 3.9 exericse

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 let persons = [
   {
@@ -79,6 +81,6 @@ app.post("/api/persons/:id", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`server running on port ${PORT}`);
